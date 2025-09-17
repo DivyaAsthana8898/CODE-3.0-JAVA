977c8c4ab40582cbf6b508974c26bec6a11b858c
DivyaAsthana8898 <divyakumar12a48@gmail.com>
2025-09-18 01:32:21 +0530

    Add files via upload

---
+++ practice29.java
@@ -0,0 +1,15 @@
+public class practice29 {
+    public static void main(String[] args) {
+        
+        int sum = 0;
+        int n = 8;//n-1
+        for ( int i = 0; i<n; i++){
+            sum = sum + (2*i);//0 + 2+ 4 +6
+
+        }
+         System.out.println("the sum of even no is");
+         System.out.println(sum);
+
+    }
+    
+}
---
+++ practice51.java
@@ -0,0 +1,20 @@
+
+ 
+public class practice51{
+    static void  Multiplication( int n){
+    for( int i = 1 ; i<=10; i++){
+        System.out.format("%d X %d = %d\n",n,i,n*i);
+        
+         
+}
+    }  
+ 
+     public static void main(String [] args){
+        Multiplication(6);
+        
+
+
+        
+     }
+    }
+     
