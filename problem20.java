8429ab679f26f32dbe67d235c114e71fb4cc0a7e
DivyaAsthana8898 <divyakumar12a48@gmail.com>
2025-09-18 22:06:17 +0530

    Add files via upload

---
+++ problem20.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class problem20 {
+    public static void main(String[] args) {
+        LocalDate d=  LocalDate.now();
+        System.out.println(d);
+        LocalTime t = LocalTime.now();
+        System.out.println(t);
+    }
+}
