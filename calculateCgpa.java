5facfc89311e3df663be4bfcf25af59823252676
Divya Asthana <divyakumar12a48@gmail.com>
2026-02-03 23:45:22 +0530

    Add files via upload

---
+++ calculateCgpa.java
@@ -0,0 +1,14 @@
+public class calculateCgpa {
+    public static void main(String[] args){
+        float gp1,gp2,gp3;
+        gp1=78;
+        gp2=98;
+        gp3=87;
+        float sum = gp1+gp2+gp3;
+        float CGPA = sum/30;
+        System.out.println("THE CGPA ACCORDING TO THE SUM OF THREE SUBJECTS IS " + CGPA);
+
+
+    }
+    
+}
