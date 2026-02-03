680e6c43ffd5e0847c3568406e6e79607e7848cc
Divya Asthana <divyakumar12a48@gmail.com>
2026-02-03 23:44:18 +0530

    Add files via upload

---
+++ NoEnteredGreaterOrNot.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class NoEnteredGreaterOrNot {
+    public static void main(String[] args) {
+        System.out.println("Enter the number");
+        Scanner sc = new Scanner(System.in);
+        int value = sc.nextInt();
+        System.out.println(value>8);
+
+        
+    }
+    
+}
