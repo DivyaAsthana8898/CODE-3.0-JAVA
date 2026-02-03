e7191f09384feac531c211794a4f01ddeb978f21
Divya Asthana <divyakumar12a48@gmail.com>
2026-02-03 23:43:49 +0530

    Add files via upload

---
+++ SumThreeNo.java
@@ -0,0 +1,13 @@
+public class SumThreeNo {
+    public static void main(String[] args){
+    int a ,b,c;
+    a= 8;
+    b=6;
+    c=9;
+    int sum = a+b+c;
+    System.out.println("The total  sum is "+sum);
+}
+}
+
+    
+
