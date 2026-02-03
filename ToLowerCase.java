61f10dc0e1381f8d90b917d677a9f4030e7d443f
Divya Asthana <divyakumar12a48@gmail.com>
2026-02-03 23:42:18 +0530

    Add files via upload

---
+++ ToLowerCase.java
@@ -0,0 +1,10 @@
+public class ToLowerCase {
+    public static void main(String[] args) {
+        String name = "JACK PARKER";
+        name = name.toLowerCase();
+        System.out.println(name);
+
+        
+    }
+    
+}
