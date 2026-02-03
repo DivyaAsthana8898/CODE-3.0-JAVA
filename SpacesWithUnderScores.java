2789a80287d019f1a5c0c1d2564e609df98cf094
Divya Asthana <divyakumar12a48@gmail.com>
2026-02-03 23:46:20 +0530

    Add files via upload

---
+++ SpacesWithUnderScores.java
@@ -0,0 +1,10 @@
+public class SpacesWithUnderScores {
+    public static void main(String[] args) {
+        String text = "To lower case";
+        text = text.replace(" ", "_");
+        System.out.println(text);
+
+        
+    }
+    
+}
