e447b228f63c3e98f47bc3d7b543ba6cdca92308
Divya Asthana <divyakumar12a48@gmail.com>
2026-02-03 23:44:45 +0530

    Add files via upload

---
+++ NoEnteredInteger.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class NoEnteredInteger {
+    public static void main(String[] args) {
+        System.out.println("Enter the number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+    
+}
